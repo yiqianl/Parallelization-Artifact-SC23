47967ae26c4f5d19bfb346d87681c74dafa2f900
y_l120 <y_l120@txstate.edu>
2023-03-28 09:56:30 -0500

    Create generate_csv.sh

---
+++ generate_csv.sh
@@ -0,0 +1,126 @@
+# convert the output to csv files
+## cuda sssp
+python convert_to_csv.py 2d sssp_2d-2e20.sym.egr_cuda.out sssp_cuda.csv sssp
+python convert_to_csv.py co sssp_coPapersDBLP.egr_cuda.out sssp_cuda.csv sssp
+python convert_to_csv.py rmat sssp_rmat22.sym.egr_cuda.out sssp_cuda.csv sssp
+python convert_to_csv.py soc sssp_soc-LiveJournal1.egr_cuda.out sssp_cuda.csv sssp
+python convert_to_csv.py ny sssp_USA-road-d.NY.egr_cuda.out sssp_cuda.csv sssp
+
+## cuda bfs
+python convert_to_csv.py 2d bfs_2d-2e20.sym.egr_cuda.out bfs_cuda.csv bfs
+python convert_to_csv.py co bfs_coPapersDBLP.egr_cuda.out bfs_cuda.csv bfs
+python convert_to_csv.py rmat bfs_rmat22.sym.egr_cuda.out bfs_cuda.csv bfs
+python convert_to_csv.py soc bfs_soc-LiveJournal1.egr_cuda.out bfs_cuda.csv bfs
+python convert_to_csv.py ny bfs_USA-road-d.NY.egr_cuda.out bfs_cuda.csv bfs
+
+## cuda cc
+python convert_to_csv.py 2d cc_2d-2e20.sym.egr_cuda.out cc_cuda.csv cc
+python convert_to_csv.py co cc_coPapersDBLP.egr_cuda.out cc_cuda.csv cc
+python convert_to_csv.py rmat cc_rmat22.sym.egr_cuda.out cc_cuda.csv cc
+python convert_to_csv.py soc cc_soc-LiveJournal1.egr_cuda.out cc_cuda.csv cc
+python convert_to_csv.py ny cc_USA-road-d.NY.egr_cuda.out cc_cuda.csv cc
+
+## cuda mis
+python convert_to_csv.py 2d mis_2d-2e20.sym.egr_cuda.out mis_cuda.csv mis
+python convert_to_csv.py co mis_coPapersDBLP.egr_cuda.out mis_cuda.csv mis
+python convert_to_csv.py rmat mis_rmat22.sym.egr_cuda.out mis_cuda.csv mis
+python convert_to_csv.py soc mis_soc-LiveJournal1.egr_cuda.out mis_cuda.csv mis
+python convert_to_csv.py ny mis_USA-road-d.NY.egr_cuda.out mis_cuda.csv mis
+
+## cuda pr
+python convert_to_csv.py 2d pr_2d-2e20.sym.egr_cuda.out pr_cuda.csv pr
+python convert_to_csv.py co pr_coPapersDBLP.egr_cuda.out pr_cuda.csv pr
+python convert_to_csv.py rmat pr_rmat22.sym.egr_cuda.out pr_cuda.csv pr
+python convert_to_csv.py soc pr_soc-LiveJournal1.egr_cuda.out pr_cuda.csv pr
+python convert_to_csv.py ny pr_USA-road-d.NY.egr_cuda.out pr_cuda.csv pr
+
+## cuda tc
+python convert_to_csv.py 2d tc_2d-2e20.sym.egr_cuda.out tc_cuda.csv tc
+python convert_to_csv.py co tc_coPapersDBLP.egr_cuda.out tc_cuda.csv tc
+python convert_to_csv.py rmat tc_rmat22.sym.egr_cuda.out tc_cuda.csv tc
+python convert_to_csv.py soc tc_soc-LiveJournal1.egr_cuda.out tc_cuda.csv tc
+python convert_to_csv.py ny tc_USA-road-d.NY.egr_cuda.out tc_cuda.csv tc
+
+## cpp sssp
+python convert_to_csv.py 2d sssp_2d-2e20.sym.egr_cpp.out sssp_cpp.csv sssp
+python convert_to_csv.py co sssp_coPapersDBLP.egr_cpp.out sssp_cpp.csv sssp
+python convert_to_csv.py rmat sssp_rmat22.sym.egr_cpp.out sssp_cpp.csv sssp
+python convert_to_csv.py soc sssp_soc-LiveJournal1.egr_cpp.out sssp_cpp.csv sssp
+python convert_to_csv.py ny sssp_USA-road-d.NY.egr_cpp.out sssp_cpp.csv sssp
+
+## cpp bfs
+python convert_to_csv.py 2d bfs_2d-2e20.sym.egr_cpp.out bfs_cpp.csv bfs
+python convert_to_csv.py co bfs_coPapersDBLP.egr_cpp.out bfs_cpp.csv bfs
+python convert_to_csv.py rmat bfs_rmat22.sym.egr_cpp.out bfs_cpp.csv bfs
+python convert_to_csv.py soc bfs_soc-LiveJournal1.egr_cpp.out bfs_cpp.csv bfs
+python convert_to_csv.py ny bfs_USA-road-d.NY.egr_cpp.out bfs_cpp.csv bfs
+
+## cpp cc
+python convert_to_csv.py 2d cc_2d-2e20.sym.egr_cpp.out cc_cpp.csv cc
+python convert_to_csv.py co cc_coPapersDBLP.egr_cpp.out cc_cpp.csv cc
+python convert_to_csv.py rmat cc_rmat22.sym.egr_cpp.out cc_cpp.csv cc
+python convert_to_csv.py soc cc_soc-LiveJournal1.egr_cpp.out cc_cpp.csv cc
+python convert_to_csv.py ny cc_USA-road-d.NY.egr_cpp.out cc_cpp.csv cc
+
+## cpp mis
+python convert_to_csv.py 2d mis_2d-2e20.sym.egr_cpp.out mis_cpp.csv mis
+python convert_to_csv.py co mis_coPapersDBLP.egr_cpp.out mis_cpp.csv mis
+python convert_to_csv.py rmat mis_rmat22.sym.egr_cpp.out mis_cpp.csv mis
+python convert_to_csv.py soc mis_soc-LiveJournal1.egr_cpp.out mis_cpp.csv mis
+python convert_to_csv.py ny mis_USA-road-d.NY.egr_cpp.out mis_cpp.csv mis
+
+## cpp pr
+python convert_to_csv.py 2d pr_2d-2e20.sym.egr_cpp.out pr_cpp.csv pr
+python convert_to_csv.py co pr_coPapersDBLP.egr_cpp.out pr_cpp.csv pr
+python convert_to_csv.py rmat pr_rmat22.sym.egr_cpp.out pr_cpp.csv pr
+python convert_to_csv.py soc pr_soc-LiveJournal1.egr_cpp.out pr_cpp.csv pr
+python convert_to_csv.py ny pr_USA-road-d.NY.egr_cpp.out pr_cpp.csv pr
+
+## cpp tc
+python convert_to_csv.py 2d tc_2d-2e20.sym.egr_cpp.out tc_cpp.csv tc
+python convert_to_csv.py co tc_coPapersDBLP.egr_cpp.out tc_cpp.csv tc
+python convert_to_csv.py rmat tc_rmat22.sym.egr_cpp.out tc_cpp.csv tc
+python convert_to_csv.py soc tc_soc-LiveJournal1.egr_cpp.out tc_cpp.csv tc
+python convert_to_csv.py ny tc_USA-road-d.NY.egr_cpp.out tc_cpp.csv tc
+
+## omp sssp
+python convert_to_csv.py 2d sssp_2d-2e20.sym.egr_omp.out sssp_omp.csv sssp
+python convert_to_csv.py co sssp_coPapersDBLP.egr_omp.out sssp_omp.csv sssp
+python convert_to_csv.py rmat sssp_rmat22.sym.egr_omp.out sssp_omp.csv sssp
+python convert_to_csv.py soc sssp_soc-LiveJournal1.egr_omp.out sssp_omp.csv sssp
+python convert_to_csv.py ny sssp_USA-road-d.NY.egr_omp.out sssp_omp.csv sssp
+
+## omp bfs
+python convert_to_csv.py 2d bfs_2d-2e20.sym.egr_omp.out bfs_omp.csv bfs
+python convert_to_csv.py co bfs_coPapersDBLP.egr_omp.out bfs_omp.csv bfs
+python convert_to_csv.py rmat bfs_rmat22.sym.egr_omp.out bfs_omp.csv bfs
+python convert_to_csv.py soc bfs_soc-LiveJournal1.egr_omp.out bfs_omp.csv bfs
+python convert_to_csv.py ny bfs_USA-road-d.NY.egr_omp.out bfs_omp.csv bfs
+
+## omp cc
+python convert_to_csv.py 2d cc_2d-2e20.sym.egr_omp.out cc_omp.csv cc
+python convert_to_csv.py co cc_coPapersDBLP.egr_omp.out cc_omp.csv cc
+python convert_to_csv.py rmat cc_rmat22.sym.egr_omp.out cc_omp.csv cc
+python convert_to_csv.py soc cc_soc-LiveJournal1.egr_omp.out cc_omp.csv cc
+python convert_to_csv.py ny cc_USA-road-d.NY.egr_omp.out cc_omp.csv cc
+
+## omp mis
+python convert_to_csv.py 2d mis_2d-2e20.sym.egr_omp.out mis_omp.csv mis
+python convert_to_csv.py co mis_coPapersDBLP.egr_omp.out mis_omp.csv mis
+python convert_to_csv.py rmat mis_rmat22.sym.egr_omp.out mis_omp.csv mis
+python convert_to_csv.py soc mis_soc-LiveJournal1.egr_omp.out mis_omp.csv mis
+python convert_to_csv.py ny mis_USA-road-d.NY.egr_omp.out mis_omp.csv mis
+
+## omp pr
+python convert_to_csv.py 2d pr_2d-2e20.sym.egr_omp.out pr_omp.csv pr
+python convert_to_csv.py co pr_coPapersDBLP.egr_omp.out pr_omp.csv pr
+python convert_to_csv.py rmat pr_rmat22.sym.egr_omp.out pr_omp.csv pr
+python convert_to_csv.py soc pr_soc-LiveJournal1.egr_omp.out pr_omp.csv pr
+python convert_to_csv.py ny pr_USA-road-d.NY.egr_omp.out pr_omp.csv pr
+
+## omp tc
+python convert_to_csv.py 2d tc_2d-2e20.sym.egr_omp.out tc_omp.csv tc
+python convert_to_csv.py co tc_coPapersDBLP.egr_omp.out tc_omp.csv tc
+python convert_to_csv.py rmat tc_rmat22.sym.egr_omp.out tc_omp.csv tc
+python convert_to_csv.py soc tc_soc-LiveJournal1.egr_omp.out tc_omp.csv tc
+python convert_to_csv.py ny tc_USA-road-d.NY.egr_omp.out tc_omp.csv tc

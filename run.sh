f8a573228f50c8ada6afaa67f512d547578f30c7
y_l120 <y_l120@txstate.edu>
2023-03-28 13:48:32 -0500

    download large inputs

--- run.sh
+++ run.sh
@@ -1,11 +1,9 @@
 #!bin/bash/
 
 # download inputs
-wget -P inputs https://userweb.cs.txstate.edu/~burtscher/research/ECLgraph/2d-2e20.sym.egr
 wget -P inputs https://userweb.cs.txstate.edu/~burtscher/research/ECLgraph/coPapersDBLP.egr
 wget -P inputs https://userweb.cs.txstate.edu/~burtscher/research/ECLgraph/rmat22.sym.egr
 wget -P inputs https://userweb.cs.txstate.edu/~burtscher/research/ECLgraph/soc-LiveJournal1.egr
-wget -P inputs https://userweb.cs.txstate.edu/~burtscher/research/ECLgraph/USA-road-d.NY.egr
 
 # run the test cases
 python run_codes.py 0 16

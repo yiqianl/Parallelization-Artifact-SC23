00c4a74af9692123caa5a478b03a90b0f707fd4a
y_l120 <y_l120@txstate.edu>
2023-03-28 09:57:09 -0500

    Add run through all inputs

--- run_codes.py
+++ run_codes.py
@@ -9,102 +9,102 @@ cpp_path = ['codes/cpp/sssp-cpp/', 'codes/cpp/bfs-cpp/', 'codes/cpp/cc-cpp/', 'c
 source = '0'
 algorithms = ['sssp', 'bfs', 'cc', 'mis', 'pr', 'tc']
 inputs_folder = 'inputs/'
+graph_names = ['2d-2e20.sym.egr', 'coPapersDBLP.egr', 'rmat22.sym.egr', 'soc-LiveJournal1.egr', 'USA-road-d.NY.egr']
 
 if __name__ == "__main__":
     # read command line
     args_val = sys.argv
-    if (len(args_val) < 3):
-        sys.exit('USAGE: verify input_path thread_count(optional)\n')
+    if (len(args_val) < 2):
+        sys.exit('USAGE: verify thread_count(optional)\n')
     verify = args_val[1]
-    input_path = inputs_folder + args_val[2]
-    thread_count = args_val[3]
+    thread_count = args_val[2]
     
-    # run cuda programs
-    # for code_path in cuda_path:
-    for i in range(len(cuda_path)):
-        code_path = cuda_path[i]
-        out_name = algorithms[i] + '_' + args_val[2] + '_cuda.out'
-        walk_code = os.walk(code_path)
-        with open(out_name, 'w') as f:
-            for root, dircs, code_files in walk_code:
-                for code_file in code_files:
-                    if code_file.endswith('.cu'):
-                        file_path = os.path.join(code_path, code_file)
-                        sys.stdout.flush()
-                        print('\ncompile : %s\n' % code_file, file=f)
-                        sys.stdout.flush()
-                        os.system('nvcc %s -O3 -arch=sm_70 -Iindigo_include -o minibenchmark' % file_path)
-                        if 'sssp' in code_path or 'bfs' in code_path:
-                            os.system('./minibenchmark %s %s %s > %s' % (input_path, source, verify, out_name))
-                        elif 'pr' in code_path:
-                            os.system('./minibenchmark %s > %s' % (input_path, out_name))
+    for graph in graph_names:
+        input_path = inputs_folder + graph
+        for i in range(len(cuda_path)):
+            code_path = cuda_path[i]
+            out_name = algorithms[i] + '_' + graph + '_cuda.out'
+            walk_code = os.walk(code_path)
+            with open(out_name, 'w') as f:
+                for root, dircs, code_files in walk_code:
+                    for code_file in code_files:
+                        if code_file.endswith('.cu'):
+                            file_path = os.path.join(code_path, code_file)
+                            sys.stdout.flush()
+                            print('\ncompile : %s\n' % code_file, file=f)
+                            sys.stdout.flush()
+                            os.system('nvcc %s -O3 -arch=sm_70 -Iindigo_include -o minibenchmark' % file_path)
+                            if 'sssp' in code_path or 'bfs' in code_path:
+                                os.system('./minibenchmark %s %s %s >> %s' % (input_path, source, verify, out_name))
+                            elif 'pr' in code_path:
+                                os.system('./minibenchmark %s >> %s' % (input_path, out_name))
+                            else:
+                                os.system('./minibenchmark %s %s >> %s' % (input_path, verify, out_name))
+                            sys.stdout.flush()
+                            if os.path.isfile('minibenchmark'):
+                                os.system('rm minibenchmark')
+                            else:
+                                sys.exit('Error: compile failed')
                         else:
-                            os.system('./minibenchmark %s %s > %s' % (input_path, verify, out_name))
-                        sys.stdout.flush()
-                        if os.path.isfile('minibenchmark'):
-                            os.system('rm minibenchmark')
-                        else:
-                            sys.exit('Error: compile failed')
-                    else:
-                        sys.exit('No cuda codes in the directory.')
+                            sys.exit('No cuda codes in the directory.')
 
-    # run cpp programs
+        # run cpp programs
     # for code_path in cpp_path:
-    for i in range(len(cpp_path)):
-        code_path = cpp_path[i]
-        out_name = algorithms[i] + '_' + args_val[2] + '_cpp.out'
-        walk_code = os.walk(code_path)
-        with open(out_name, 'w') as f:
+        for i in range(len(cpp_path)):
+            code_path = cpp_path[i]
+            out_name = algorithms[i] + '_' + graph + '_cpp.out'
+            walk_code = os.walk(code_path)
+            with open(out_name, 'w') as f:
 
-            for root, dircs, code_files in walk_code:
-                for code_file in code_files:
-                    if code_file.endswith('.cpp'):
-                        file_path = os.path.join(code_path, code_file)
-                        sys.stdout.flush()
-                        print('\ncompile : %s\n' % code_file)
-                        sys.stdout.flush()
-                        os.system('g++ %s -O3 -pthread -Iindigo_include -o minibenchmark' % file_path)
-                        if 'sssp' in code_path or 'bfs' in code_path:
-                            os.system('./minibenchmark %s %s %s %s > %s' % (input_path, source, verify, thread_count, out_name))
-                        elif 'pr' in code_path:
-                            os.system('./minibenchmark %s %s > %s' % (input_path, thread_count, out_name))
-                        else:
-                            os.system('./minibenchmark %s %s %s > %s' % (input_path, verify, thread_count, out_name))
+                for root, dircs, code_files in walk_code:
+                    for code_file in code_files:
+                        if code_file.endswith('.cpp'):
+                            file_path = os.path.join(code_path, code_file)
+                            sys.stdout.flush()
+                            print('\ncompile : %s\n' % code_file)
+                            sys.stdout.flush()
+                            os.system('g++ %s -O3 -pthread -Iindigo_include -o minibenchmark' % file_path)
+                            if 'sssp' in code_path or 'bfs' in code_path:
+                                os.system('./minibenchmark %s %s %s %s >> %s' % (input_path, source, verify, thread_count, out_name))
+                            elif 'pr' in code_path:
+                                os.system('./minibenchmark %s %s >> %s' % (input_path, thread_count, out_name))
+                            else:
+                                os.system('./minibenchmark %s %s %s >> %s' % (input_path, verify, thread_count, out_name))
                     # os.system('./minibenchmark %s %s %s' % (input_path, verify, thread_count))
-                        sys.stdout.flush()
-                        if os.path.isfile('minibenchmark'):
-                            os.system('rm minibenchmark')
+                            sys.stdout.flush()
+                            if os.path.isfile('minibenchmark'):
+                                os.system('rm minibenchmark')
+                            else:
+                                sys.exit('Error: compile failed')
                         else:
-                            sys.exit('Error: compile failed')
-                    else:
-                        sys.exit('No C++ threads codes in the directory.')
+                            sys.exit('No C++ threads codes in the directory.')
 
     # for code_path in omp_path:
-    for i in range(len(omp_path)):
-        code_path = omp_path[i]
-        out_name = algorithms[i] + '_' + args_val[2] + '_omp.out'
-        walk_code = os.walk(code_path)
-        with open(out_name, 'w') as f:
-            for root, dircs, code_files in walk_code:
-                for code_file in code_files:
-                    if code_file.endswith('.cpp'):
-                        file_path = os.path.join(code_path, code_file)
-                        sys.stdout.flush()
-                        print('\ncompile : %s\n' % code_file)
-                        sys.stdout.flush()
-                        os.system('g++ %s -O3 -fopenmp -Iindigo_include -o minibenchmark' % file_path)
-                        os.system('export OMP_NUM_THREADS=%s' % thread_count)
-                        if 'sssp' in code_path or 'bfs' in code_path:
-                            os.system('./minibenchmark %s %s %s > %s' % (input_path, source, verify, out_name))
-                        elif 'pr' in code_path:
-                            os.system('./minibenchmark %s > %s' % (input_path, out_name))
-                        else:
-                            os.system('./minibenchmark %s %s > %s' % (input_path, verify, out_name))
+        for i in range(len(omp_path)):
+            code_path = omp_path[i]
+            out_name = algorithms[i] + '_' + graph + '_omp.out'
+            walk_code = os.walk(code_path)
+            with open(out_name, 'w') as f:
+                for root, dircs, code_files in walk_code:
+                    for code_file in code_files:
+                        if code_file.endswith('.cpp'):
+                            file_path = os.path.join(code_path, code_file)
+                            sys.stdout.flush()
+                            print('\ncompile : %s\n' % code_file)
+                            sys.stdout.flush()
+                            os.system('g++ %s -O3 -fopenmp -Iindigo_include -o minibenchmark' % file_path)
+                            os.system('export OMP_NUM_THREADS=%s' % thread_count)
+                            if 'sssp' in code_path or 'bfs' in code_path:
+                                os.system('./minibenchmark %s %s %s >> %s' % (input_path, source, verify, out_name))
+                            elif 'pr' in code_path:
+                                os.system('./minibenchmark %s >> %s' % (input_path, out_name))
+                            else:
+                                os.system('./minibenchmark %s %s >> %s' % (input_path, verify, out_name))
                     # os.system('./minibenchmark %s %s' % (input_path, verify))
-                        sys.stdout.flush()
-                        if os.path.isfile('minibenchmark'):
-                            os.system('rm minibenchmark')
+                            sys.stdout.flush()
+                            if os.path.isfile('minibenchmark'):
+                                os.system('rm minibenchmark')
+                            else:
+                                sys.exit('Error: compile failed')
                         else:
-                            sys.exit('Error: compile failed')
-                    else:
-                        sys.exit('No omp codes in the directory.')
+                            sys.exit('No omp codes in the directory.')

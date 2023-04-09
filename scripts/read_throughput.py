4f68a96fa1ab2ee8c3cd64e80a3860fdcdb4bcca
y_l120 <y_l120@txstate.edu>
2023-04-08 23:21:15 -0500

    Update scripts

--- scripts/read_throughput.py
+++ scripts/read_throughput.py
@@ -7,7 +7,7 @@ args = sys.argv
 if (len(args) <= 4):
     sys.exit('USAGE: prefix input_file out_file algo_name \n')
 
-input_file_path = "../" + args[2]
+input_file_path = args[2]
 in_file = open(input_file_path, 'r')
 in_lines = in_file.readlines()
 
@@ -18,8 +18,8 @@ algo_name = args[4]
 runtimes = []
 rows = []
 one_row = ['', '']
-idx = 0
-counter = 0
+idx1 = 0
+idx2 = 0
 
 header = True
 if os.path.isfile(out_file):
@@ -36,13 +36,24 @@ with file as csvfile:
 
     for l in in_lines:
         if 'compile' in l:
-            str_list = l.split(' ')
-            one_row[0] = (prefix + str_list[-1]).replace('\n', '')
-            rows.append(one_row)
+            # str_list = l.split(' ')
+            # one_row[0] = (prefix + str_list[-1]).replace('\n', '')
+            # rows.append(one_row)
+            if idx1 <= len(rows):
+                rows.append(one_row)
+            rows[idx1][0] = (prefix + l.split(' ')[-1]).replace('\n', '')
+            idx1 += 1
         if 'Throughput' in l:
-            str_list = l.split(' ')
-            rows[idx][1] = (str_list[-2])
-            csvwriter.writerow(rows[idx])
-            idx += 1
+            # str_list = l.split(' ')
+            # rows[idx][1] = (str_list[-2])
+            # csvwriter.writerow(rows[idx])
+            # idx += 1
+            if idx2 <= len(rows):
+                rows.append(one_row)
+            rows[idx2][1] = (l.split(' ')[-2])
+            idx2 += 1
 
-print(idx)
+    for row in rows:
+        csvwriter.writerow(row)
+
+# print(idx)
--- scripts/run_cpp_codes.py
+++ scripts/run_cpp_codes.py
@@ -3,11 +3,12 @@
 import os
 import sys
 
-cpp_path = ['../codes/cpp/sssp-cpp/', '../codes/cpp/bfs-cpp/', '../codes/cpp/cc-cpp/', '../codes/cpp/mis-cpp/', '../codes/cpp/pr-cpp/', '../codes/cpp/tc-cpp/']
+cpp_path = ['codes/cpp/sssp-cpp/', 'codes/cpp/bfs-cpp/', 'codes/cpp/cc-cpp/', 'codes/cpp/mis-cpp/', 'codes/cpp/pr-cpp/', 'codes/cpp/tc-cpp/']
 source = '0'
 algorithms = ['sssp', 'bfs', 'cc', 'mis', 'pr', 'tc']
 inputs_folder = '../inputs/'
-cpp_out = '../throughputs/cpp'
+out_dir = 'throughputs/'
+cpp_out = 'throughputs/cpp'
 graph_names = ['2d-2e20.sym.egr', 'coPapersDBLP.egr', 'rmat22.sym.egr', 'soc-LiveJournal1.egr', 'USA-road-d.NY.egr']
 total_num_cpp = 176
 counter = 0
@@ -19,7 +20,12 @@ if __name__ == "__main__":
         sys.exit('USAGE: verify thread_count(optional)\n')
     verify = args_val[1]
     thread_count = args_val[2]
-    
+
+    # create output directory
+    if os.path.exists(out_dir):
+        os.rmdir(out_dir)
+    os.mkdir(out_dir)
+
     for graph in graph_names:
         input_path = inputs_folder + graph
 
@@ -27,16 +33,21 @@ if __name__ == "__main__":
         for i in range(len(cpp_path)):
             code_path = cpp_path[i] 
             walk_code = os.walk(code_path)
+
+            # create cpp out directory
+            if os.path.exists(cpp_out):
+                os.rmdir(cpp_out)
+            os.mkdir(cpp_out)
             out_name = cpp_out + algorithms[i] + '_' + graph + '_cpp.out'
             
             if os.path.isfile(out_name):
                 os.remove(out_name)
-                
+
             for root, dircs, code_files in walk_code:
                 for code_file in code_files:
                     if code_file.endswith('.cpp'):
                         counter += 1
-                        print("running %s\n %d out of %d programs, %d out of %d inputs" % (code_file, counter, total_num_cpp, graph_names.index(graph) + 1, len(graph_names)))
+                        print("running %s\n %dout of %d programs, %d out of %d inputs" % (code_file, counter, total_num_cpp, graph_names.index(graph) + 1, len(graph_names)))
                         with open(out_name, 'a') as f:
                             file_path = os.path.join(code_path, code_file)
                             sys.stdout.flush()

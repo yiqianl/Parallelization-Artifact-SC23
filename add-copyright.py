4abf13ac4d5a0da44617b26851461214c2c2350e
y_l120 <y_l120@txstate.edu>
2023-06-23 17:07:32 -0500

    Add copyright

---
+++ add-copyright.py
@@ -0,0 +1,55 @@
+import os
+import sys
+
+directory = sys.argv[1]  
+
+paragraph = '''
+/*
+This file is part of the Indigo2 benchmark suite version 0.9.
+
+Copyright (c) 2023, Yiqian Liu, Noushin Azami, Avery Vanausdal, and Martin Burtscher
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+   contributors may be used to endorse or promote products derived from
+   this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+URL: The latest version of this code is available at https://cs.txstate.edu/~burtscher/research/Indigo2Suite/.
+
+Publication: This work is described in detail in the following paper.
+
+Yiqian Liu, Noushin Azami, Avery Vanausdal, and Martin Burtscher. "Choosing the Best Parallelization and Implementation Styles for Graph Analytics Codes: Lessons Learned from 1106 Programs." Proceedings of the 2023 ACM/IEEE International Conference for High Performance Computing, Networking, Storage, and Analysis. November 2023.
+*/
+'''
+
+# Get a list of all files in the directory
+files = os.listdir(directory)
+
+# Iterate over each file
+for file_name in files:
+    if file_name.endswith('.cpp') or file_name.endswith('.cu'):  # Modify the file extension as per your requirement
+        file_path = os.path.join(directory, file_name)
+        with open(file_path, 'r+') as file:
+            content = file.read()
+            file.seek(0, 0)  # Move the file pointer to the beginning
+            file.write(paragraph + '\n' + content)

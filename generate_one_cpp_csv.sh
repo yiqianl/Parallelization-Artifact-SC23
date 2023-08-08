53e35fb0de4983bc4a9cd1635d2d71dc4f4fe6c1
yiqianl <76009606+yiqianl@users.noreply.github.com>
2023-08-08 11:53:24 -0500

    Update generate_one_cpp_csv.sh
    Write results to data/ folder.

--- generate_one_cpp_csv.sh
+++ generate_one_cpp_csv.sh
@@ -2,19 +2,19 @@
 
 # convert the output to csv files
 ## cpp sssp
-python scripts/read_throughput.py soc throughputs/cpp/sssp_soc-LiveJournal1.egr_cpp.out sssp_cpp.csv sssp
+python scripts/read_throughput.py soc throughputs/cpp/sssp_soc-LiveJournal1.egr_cpp.out data/sssp_cpp.csv sssp
 
 ## cpp bfs
-python scripts/read_throughput.py soc throughputs/cpp/bfs_soc-LiveJournal1.egr_cpp.out bfs_cpp.csv bfs
+python scripts/read_throughput.py soc throughputs/cpp/bfs_soc-LiveJournal1.egr_cpp.out data/bfs_cpp.csv bfs
 
 ## cpp cc
-python scripts/read_throughput.py soc throughputs/cpp/cc_soc-LiveJournal1.egr_cpp.out cc_cpp.csv cc
+python scripts/read_throughput.py soc throughputs/cpp/cc_soc-LiveJournal1.egr_cpp.out data/cc_cpp.csv cc
 
 ## cpp mis
-python scripts/read_throughput.py soc throughputs/cpp/mis_soc-LiveJournal1.egr_cpp.out mis_cpp.csv mis
+python scripts/read_throughput.py soc throughputs/cpp/mis_soc-LiveJournal1.egr_cpp.out data/mis_cpp.csv mis
 
 ## cpp pr
-python scripts/read_throughput.py soc throughputs/cpp/pr_soc-LiveJournal1.egr_cpp.out pr_cpp.csv pr
+python scripts/read_throughput.py soc throughputs/cpp/pr_soc-LiveJournal1.egr_cpp.out data/pr_cpp.csv pr
 
 ## cpp tc
-python scripts/read_throughput.py soc throughputs/cpp/tc_soc-LiveJournal1.egr_cpp.out tc_cpp.csv tc
+python scripts/read_throughput.py soc throughputs/cpp/tc_soc-LiveJournal1.egr_cpp.out data/tc_cpp.csv tc
